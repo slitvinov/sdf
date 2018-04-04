2ef94891e7606a6c164d7d1e936347c8af2c76ea
Sergey Litvinov <slitvinov@gmail.com>
2018-04-04 12:08:06 +0200

    add error check

--- lib/file/inc/sdf_file.h
+++ lib/file/inc/sdf_file.h
@@ -1,10 +1,14 @@
 typedef struct SDFFile SDFFile;
 
 enum {FILE_OK, FILE_SIZE};
-int sdf_file_ini(int nx, int ny, int nz, /**/ SDFFile**);
+int sdf_file_ini(int nx, int ny, int nz,
+                 double ex, double ey, double ez,
+                 /**/ SDFFile**);
 int sdf_file_fin(SDFFile*);
 
-int sdf_file_n  (SDFFile*, /**/ int *n);
+int sdf_file_n       (SDFFile*, /**/ int *n);
+int sdf_file_volume  (SDFFile*, /**/ double *volume);
+
 int sdf_file_xyz(SDFFile*, int i, /**/ double *x, double *y, double *z);
 int sdf_file_set(SDFFile*, int i, double val);
 
--- lib/file/src/main.c
+++ lib/file/src/main.c
@@ -4,22 +4,64 @@
 #include "sdf_file.h"
 
 struct SDFFile {
+    double ex, ey, ez; /* extent */
     int nx, ny, nz;
     float *D;
 };
 
-int sdf_file_ini(int nx, int ny, int nz, /**/ SDFFile **pq) {
+int sdf_file_ini(int nx, int ny, int nz,
+                 double ex, double ey, double ez,
+                 /**/ SDFFile **pq) {
     int n;
     SDFFile *q;
     EMALLOC(1, &q);
     n = nx * ny * nz;
     EMALLOC(n, &q->D);
+    q->nx = nx; q->ny = ny; q->nz = nz;
+    q->ex = ex; q->ey = ey; q->ez = ez;
     *pq = q;
-    return FILE_OK;    
+    return FILE_OK;
 }
 
 int sdf_file_fin(SDFFile *q) {
     EFREE(q->D);
     EFREE(q);
+    return FILE_OK;
+}
+
+int sdf_file_n(SDFFile *q, int *pn) {
+    int nx, ny, nz, n;
+    nx = q->nx; ny = q->ny; nz = q->nz;
+    n = nx * ny * nz;
+    *pn = n;
+    return FILE_OK;
+}
+
+int sdf_file_volume(SDFFile *q, double *pvolume) {
+    double ex, ey, ez, volume;
+    ex = q->ex; ey = q->ey; ez = q->ez;
+    volume = ex * ey * ez;
+    *pvolume = volume;
+    return FILE_OK;
+}
+
+int sdf_file_xyz(SDFFile *q, int i, double *px, double *py, double *pz) {
+    int nx, ny, nz, n;
+    double x, y, z;
+    nx = q->nx; ny = q->ny; nz = q->nz;
+    n = nx * ny * nz;
+    if (i < 0) {
+        fprintf(stderr, "i=%d < 0\n", i);
+        return FILE_SIZE;
+    }
+
+    if (i >= n) {
+        fprintf(stderr, "i=%d >= n=%d\n", i, n);
+        return FILE_SIZE;
+    }
+
+    z = 10;
+
+    *px = x; *py = y; *pz = z;
     return FILE_OK;
 }
