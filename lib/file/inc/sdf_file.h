0ab3b312598824aa1df1a86358f4699f247fde42
Sergey Litvinov <slitvinov@gmail.com>
2018-04-04 11:32:08 +0200

    add decl for sdf_file

--- lib/file/inc/sdf_file.h
+++ lib/file/inc/sdf_file.h
@@ -1,4 +1,8 @@
 typedef struct SDFFile SDFFile;
 
-int sdf_file_ini(SDFFile**);
+int sdf_file_ini(int nx, int ny, int nz, /**/ SDFFile**);
 int sdf_file_fin(SDFFile*);
+
+int sdf_file_n  (SDFFile*, /**/ int *n);
+int sdf_file_xyz(SDFFile*, int i, /**/ double *x, double *y, double *z);
+int sdf_file_write(SDFFile*, const char *path);
