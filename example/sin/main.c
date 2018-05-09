a0207c0cf99fe2bcb69f264cde1d2e5b0ecf2d32
Sergey Litvinov <slitvinov@gmail.com>
2018-05-09 16:45:14 +0200

    use kernel_conf in main.c

--- example/sin/main.c
+++ example/sin/main.c
@@ -4,6 +4,7 @@
 
 #include <sdf_integration.h>
 #include <sdf_kernel.h>
+#include <sdf_kernel_conf.h>
 #include <sdf_file.h>
 #include <sdf_root.h>
 
@@ -12,8 +13,6 @@
 static double ex, ey, ez, cx, cy, cz;
 static double amp_l = 0.0;
 static double amp_w = 0.4;
-
-static double sq(double x) { return x*x; }
 
 static double fx(double t) { return ex * t; }
 static double fy(double t) {
@@ -24,40 +23,13 @@ static double  I(double t) {
     return 1.0 + amp_w*sin(2*PI*t);
 };
 
-typedef struct Kernel {
-    SDFKernel *kernel; /* inout */
-    SDFIntegration *integration;
-    double a, b;
-} Kernel;
-
-static void kern_conf(Kernel *q, double x, double y, double z, /**/ double *palpha, double *pbeta) {
-    double cutoff, alpha, beta, a, b;
-    SDFIntegration *integration;
-    SDFKernel *kernel;
-    integration = q->integration; kernel = q->kernel;
-    a = q->a; b = q->b;
-    sdf_kernel_xyz(kernel, x, y, z);
-    cutoff = 10.0;
-    sdf_kernel_cutoff(kernel, cutoff);
-    sdf_integration_apply(integration,
-                          sdf_kernel_w, kernel,
-                          a, b,
-                          &alpha);
-    sdf_integration_apply(integration,
-                          sdf_kernel_dw, kernel,
-                          a, b,
-                          &beta);
-    printf("alpha, beta : %g %g\n", alpha, beta);
-    *palpha = alpha; *pbeta = beta;
-}
-
 int main() {
     SDFIntegration *integration;
-    Kernel K;
     SDFKernel *kernel;
+    SDFKernelConf *kernel_conf;
     SDFFile *file;
     int nx, ny, nz, n, i, s;
-    double r, v, g, x, y, z, x0, y0, z0, alpha, beta, val;
+    double val, lo, hi, x, y, z, x0, y0, z0, A, C, cutoff;
     double a, b, res;
     const char *o = "sdf.dat";
 
@@ -66,15 +38,18 @@ int main() {
 
     cx = ex/2; cy = ey/2; cz = ez/2;
     a = -1; b = 2;
-    x0 = cx;   y0 = cy; z0 = cz + 0.2*ez;
+    x0 = cx;   y0 = cy; z0 = cz + 0.1*ez;
 
     sdf_file_ini(nx, ny, nz, ex, ey, ez, &file);
     sdf_file_n(file, &n);
     sdf_integration_ini(&integration);
     sdf_kernel_ini(fx, fy, fz, I, &kernel);
+    sdf_kernel_xyz(kernel, x0, y0, z0);
 
-    K.kernel = kernel; K.a = a; K.b = b; K.integration = integration;
-    kern_conf(&K, x0, y0, z0, /**/ &alpha, &beta);
+    sdf_kernel_conf_ini(kernel, a, b, /**/ &kernel_conf);
+    lo = 4; hi = 20;
+    sdf_kernel_conf_apply(kernel_conf, lo, hi, /**/ &cutoff, &A, &C);
+    sdf_kernel_cutoff(kernel, cutoff);
 
     for (i = 0; i < n; i++) {
         s = sdf_file_xyz(file, i, /**/ &x, &y, &z);
@@ -83,11 +58,8 @@ int main() {
             exit(2);
         }
         sdf_kernel_xyz(kernel, x, y, z);
-        sdf_integration_apply(integration,
-                              sdf_kernel_w, kernel,
-                              a, b,
-                              &res);
-        val = alpha/beta - res/beta;
+        sdf_integration_apply(integration, sdf_kernel_w, kernel, a, b, &res);
+        val = A*res  - C;
         sdf_file_set(file, i,  val);
 //        printf("%g %g %g %g %g\n", x, y, z, val, res);
     }
@@ -96,4 +68,5 @@ int main() {
     sdf_kernel_fin(kernel);
     sdf_integration_fin(integration);
     sdf_file_fin(file);
+    sdf_kernel_conf_fin(kernel_conf);
 }
